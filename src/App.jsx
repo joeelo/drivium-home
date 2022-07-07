f89c32183e488b8c1b1795cc1eba3b989afeb1ea
joeelo <josephclorenzo@gmail.com>
2022-07-07 12:53:12 -0700

    added driv page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import styled from "styled-components"
 function App() {
   return (
     <VideoBackground>
+      <Div>
+        <P>Freddy G Leiva - Co-Founder/CEO</P>
+        <P>Joe Cash Lorenzo - Co-Founder/CTO</P>
+        <P>Steven Money Peniche - Head of Product</P>
+        <P>Mike Maybach Elias - Head of Public Relations</P>
+        <P>Will Music Rockafeller - Head of Music</P>
+      </Div>
       <Video src="/production-ID-4990242.mp4" autoPlay loop />
       <H1>DRIVIUM</H1>
     </VideoBackground>
@@ -17,7 +24,6 @@ const VideoBackground = styled.div`
   min-height: 100vh;
   position: relative;
   display: flex;
-  background-color: blue;
   align-items: center;
   justify-content: center;
   max-width: 100vw;
@@ -31,12 +37,28 @@ const Video = styled.video`
   min-width: 100vw;
   min-height: 100vh;
   max-height: 100vh;
+  z-index: -1;
 `
 
 const H1 = styled.h1`
-  font-weight: 800;
+  font-weight: 900;
   color: white;
   font-size: 128px;
   z-index: 1000;
   font-family: "Montserrat", sans-serif;
+`
+
+const Div = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  background-color: black;
+  padding: 10px;
+  border-radius: 4px;
+`
+
+const P = styled.p`
+  color: white;
+  font-size: 18px;
+  margin: 0;
 `
